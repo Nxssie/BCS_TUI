36e9e9d980950d5dbff247345de8cba89a6794f4
Carlos <nxssiedev@gmail.com>
2021-02-22 18:37:57 +0000

    FIX MSA CHANGE MSA import way FIX IGV CHANGE IGV dataset to default

--- src/app/components/igv/igv.component.ts
+++ src/app/components/igv/igv.component.ts
@@ -9,37 +9,25 @@ import * as igv from 'node_modules/igv/dist/igv.min.js';
 export class IGVComponent implements OnInit {
   constructor() {}
 
-  
-
   ngOnInit(): void {
-
-    const path ="../../../assets/seq-data/set1/";
-
-    var igvDiv = document.getElementById("igv-div");
-      var options =
+    var igvDiv = document.getElementById('igv-div');
+    var options = {
+      genome: "hg38",
+      locus: "chr8:127,736,588-127,739,371",
+      tracks: [
         {
-          locus:"Untitled",
-          reference: {
-            fastaURL: path + "sequences.fasta",
-            indexed: false
-          },
-          tracks: [
-            {
-              name: "Alignment",
-              type: "annotation",
-              format: "aln",
-              sourceType: "file",
-              url: path + "alignment.aln",
-              indexed: false,
-              displayMode: "EXPANDED"
-            }
-          ]        
-        };
+          name: 'HG00103',
+          url:
+            'https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram',
+          indexURL:
+            'https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram.crai',
+          format: 'cram',
+        },
+      ],
+    };
 
-        igv.createBrowser(igvDiv, options)
-                .then(function () {
-                    console.log("Created IGV browser");
-                })
-    
+    igv.createBrowser(igvDiv, options).then(function () {
+      console.log('Created IGV browser');
+    });
   }
 }
--- src/app/components/msa/msa.component.ts
+++ src/app/components/msa/msa.component.ts
@@ -1,38 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+declare let msa: any;
 
 @Component({
   selector: 'app-msa',
   templateUrl: './msa.component.html',
   styleUrls: ['./msa.component.css'],
 })
+
 export class MsaComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    var msa = require('msa');
-
     var opts = {
       el: document.getElementById('msaTable'),
+      importURL: '../../../assets/seq-data/set1/sequences.fasta',
+      boostrapMenu: true,
       conf: {
         registerMouseHover: false,
         registerMouseClicks: true,
+        importProxy: "https://cors-anywhere.herokuapp.com/",
         eventBus: true,
         alphabetSize: 20,
         dropImport: false,
         debug: false,
-        hasRef: true, // hasReference,
-        manualRendering: false, // manually control the render (not recommened)
-      },
-      colorscheme: {
-        scheme: 'taylor', // name of your color scheme
+        hasRef: false, // hasReference,
+        manualRendering: false // manually control the render (not recommened)
+    },
+    colorscheme: {
+        scheme: "taylor", // name of your color scheme
         colorBackground: true, // otherwise only the text will be colored
         showLowerCase: true, // used to hide and show lowercase chars in the overviewbox
-        opacity: 0.6, //opacity for the residues
-      },
-      columns: {
-        hidden: [], // hidden columns
-      },
-      vis: {
+        opacity: 0.6 //opacity for the residues
+    },
+    columns: {
+      hidden: [] // hidden columns
+    },
+    vis: {
         sequences: true,
         markers: true,
         metacell: false,
@@ -41,60 +44,59 @@ export class MsaComponent implements OnInit {
         seqlogo: false,
         gapHeader: false,
         leftHeader: true,
-
+    
         // about the labels
         labels: true,
         labelName: true,
-        labelId: true,
+        labelId: false,
         labelPartition: false,
         labelCheckbox: false,
-
+    
         // meta stuff
         metaGaps: true,
         metaIdentity: true,
-        metaLinks: true,
-      },
-      zoomer: {
+        metaLinks: true
+    },
+    zoomer: {
         // general
-        alignmentWidth: 'auto',
+        alignmentWidth: "auto",
         alignmentHeight: 225,
         columnWidth: 15,
         rowHeight: 15,
         autoResize: true, // only for the width
-
+    
         // labels
         textVisible: true,
-        labelIdLength: 30,
+        labelIdLength: 160,
         labelNameLength: 100,
         labelPartLength: 15,
         labelCheckLength: 15,
         labelFontsize: 13,
-        labelLineHeight: '13px',
-
+        labelLineHeight: "13px",
+    
         // marker
-        markerFontsize: '10px',
+        markerFontsize: "10px",
         stepSize: 1,
         markerStepSize: 2,
         markerHeight: 20,
-
+    
         // canvas
-        residueFont: '13', //in px
+        residueFont: "13", //in px
         canvasEventScale: 1,
-
+    
         // overview box
         boxRectHeight: 2,
         boxRectWidth: 2,
         overviewboxPaddingTop: 10,
-
+    
         // meta cell
         metaGapWidth: 35,
         metaIdentWidth: 40,
-        metaLinksWidth: 25,
-      },
-      importURL: '../../../assets/seq-data/set1/sequences.fasta',
+        metaLinksWidth: 25
+    }
     };
 
-    var m = new msa.msa(opts);
+    var m = new msa(opts);
     m.render();
 
     var defaultURL =
